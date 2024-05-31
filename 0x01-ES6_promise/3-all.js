a76959fc6e2115a3e7f99cea251b81e059a8f1a3
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-01 02:06:03 +0300

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto } from "./utils.js"
+import { createUser } from "./utils.js"
+
+export default function handleProfileSignup() {
+    const uploadPhotoResponse = uploadPhoto();
+    const uploadcreateUser = createUser();
+
+    Promise.all([
+        uploadPhotoResponse,
+        uploadcreateUser
+    ])
+        .then(responses => console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`))
+        .catch(() => console.log('Signup system offline'))
+}
