4cecacab95bce5cbf902be69e73c94fcafc106f1
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-01 04:38:46 +0300

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((results) => results.map(
+      (obj) => (obj.value
+        ? obj
+        : { status: obj.status, value: obj.reason }),
+    ));
+}
