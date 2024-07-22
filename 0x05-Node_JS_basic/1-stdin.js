a52b25fa0a226fa26f0889851cd01ce91d64dc2a
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-22 21:07:41 +0300

    task 1. Using Process stdin   (Extra newline)

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utsf8');
+process.stdin.on('data', (input) => {
+  // input = input.trim();
+  console.log(`Your name is: ${input}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
