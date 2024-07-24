657487cb1773f9caabe02520868cf5bbe38a1e1e
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-24 04:41:36 +0300

    task 3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+
+function countStudents(filePath) {
+  return fs.readFile(filePath, 'utf8')
+    .then((data) => {
+      const rows = data.split('\n').filter((row) => row.trim() !== '');
+      const headers = rows[0].split(',');
+
+      const studentsByField = {};
+
+      rows.slice(1).forEach((row) => {
+        const values = row.split(',');
+        const field = values[headers.indexOf('field')];
+        const firstname = values[headers.indexOf('firstname')];
+
+        if (field && firstname) {
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstname);
+        }
+      });
+
+      console.log(`Number of students: ${rows.length - 1}`);
+
+      Object.keys(studentsByField).forEach((field) => {
+        console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+      });
+
+      return rows.length - 1;
+    })
+    .catch((err) => {
+      if (err.code === 'ENOENT') {
+        throw new Error('Cannot load the database');
+      }
+      throw err;
+    });
+}
+
+module.exports = countStudents;
