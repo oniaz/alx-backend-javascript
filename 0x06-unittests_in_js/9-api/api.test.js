1b25f88b8c22bf499a0c694d4d8f5aaf25be12ae
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-26 02:47:46 +0300

    task 9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,64 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('GET /', function () {
+  it('should return a status code of 200', function (done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      if (err) return done(err);
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct response message', function (done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      if (err) return done(err);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should have a content-type of text/html', function (done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      if (err) return done(err);
+      expect(res.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+});
+
+
+describe('GET /cart/:id', function () {
+  it('should return a status code of 200 when id is a number', function (done) {
+    request.get('http://localhost:7865/cart/89', (err, res, body) => {
+      if (err) return done(err);
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct response message', function (done) {
+    request.get('http://localhost:7865/cart/89', (err, res, body) => {
+      if (err) return done(err);
+      expect(body).to.equal('Payment methods for cart 89');
+      done();
+    });
+  });
+
+  it('should have a content-type of text/html', function (done) {
+    request.get('http://localhost:7865/cart/89', (err, res, body) => {
+      if (err) return done(err);
+      expect(res.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+
+  it('should return a status code of 200', function (done) {
+    request.get('http://localhost:7865/notanumber', (err, res, body) => {
+      if (err) return done(err);
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+});
