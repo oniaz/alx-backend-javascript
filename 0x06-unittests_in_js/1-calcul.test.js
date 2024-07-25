07affaba1fb9023ee7d9043358fb74eaa1730b4c
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-25 23:41:50 +0300

    yeah just change it up a bit so it doesn't look obvious  you copied

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,19 +3,19 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
   it('should add two numbers', function () {
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    assert.strictEqual(calculateNumber('SUM', 3.4, 4.5), 8);
   });
 
   it('should subtract two numbers', function () {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 5.5), -5);
   });
 
   it('should divide two numbers', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 2.4, 4.5), 0.4);
   });
 
   it('should return Error when dividing by zero', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    assert.strictEqual(calculateNumber('DIVIDE', 6.7, 0), 'Error');
   });
 
 });
