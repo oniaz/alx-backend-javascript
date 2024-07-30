26a30497dfa4d1ef0f3e79dcf34a153471e4dedf
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-30 03:27:35 +0300

    task 7 done with a clear head (just watched endeavour eeeeeeek (i love it so much (also sketched an excellent portrait of morse in only  a couple minutes - one of my best fr)))

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+function countStudents(filePath) {
+  return fs.readFile(filePath, 'utf8')
+    .then((data) => {
+      const rows = data.split('\n').filter((row) => row.trim() !== '');
+      const headers = rows[0].split(',');
+      const studentsByField = {};
+
+      rows.slice(1).forEach((row) => {
+        const values = row.split(',');
+        const field = values[headers.indexOf('field')];
+        const firstname = values[headers.indexOf('firstname')];
+
+        if (field && firstname) {
+          if (!studentsByField[field]) {
+            studentsByField[field] = [];
+          }
+          studentsByField[field].push(firstname);
+        }
+      });
+
+      let output = `Number of students: ${rows.length - 1}`;
+
+      Object.keys(studentsByField).forEach((field) => {
+        output += `\nNumber of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`;
+      });
+
+      return output;
+    })
+    .catch((err) => {
+      if (err.code === 'ENOENT') {
+        return 'Cannot load the database';
+      }
+      throw err;
+    });
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+app.listen(1245, () => {
+});
+
+module.exports = app;
